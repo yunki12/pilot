cf07fd6613b1cb032bb64b3f7ea6f128ede03524
gibum <yoongibum@blockcom.co.kr>
2018-12-17 14:19:41 +0900

    Initial commit

---
+++ src/main/java/com/pilot/pilot/com/pilot/pilot/common/PilotListener.java
@@ -0,0 +1,22 @@
+package com.pilot.pilot.com.pilot.pilot.common;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by 2018-12-17
+ *
+ * @author yoongibum
+ */
+
+@Component
+public class PilotListener implements ApplicationListener<ContextRefreshedEvent>{
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.println("onApplicationEvent");
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
+	}
+}
---
+++ src/test/java/com/pilot/pilot/PilotApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pilot.pilot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PilotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
